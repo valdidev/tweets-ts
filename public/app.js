54c822b2e89e5d26fe2f30842791f0951e43a6c5
“valdidev” <iamvaldidev@gmail.com>
2023-01-24 10:10:17 +0100

    feat: functions of structure

--- public/app.js
+++ public/app.js
@@ -1,1 +1,33 @@
 "use strict";
+function createMainTweet() {
+    const id = crypto.randomUUID();
+    const tweet = createTweet();
+    return {
+        id,
+        tweets: [tweet],
+    };
+}
+function createTweet() {
+    const id = crypto.randomUUID();
+    const message = "";
+    return {
+        id,
+        message,
+    };
+}
+function renderView(tweetView) {
+    var _a;
+    let view = document.querySelector("#container-" + tweetView.id);
+    if (view) {
+        view.innerHTML = "";
+    }
+    else {
+        view = document.createElement("div");
+        view.id = "container-" + tweetView.id;
+        view.classList.add("mainContainer");
+        (_a = document.querySelector("#tweets")) === null || _a === void 0 ? void 0 : _a.append(view);
+    }
+    for (const tweet of tweetView.tweets) {
+        //renderTweet
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -12,4 +12,39 @@ interface TweetView {
   tweets: Tweet[];
 }
 
+function createMainTweet(): TweetView {
+  const id = crypto.randomUUID();
+  const tweet = createTweet();
 
+  return {
+    id,
+    tweets: [tweet],
+  };
+}
+
+function createTweet(): Tweet {
+  const id = crypto.randomUUID();
+  const message = "";
+
+  return {
+    id,
+    message,
+  };
+}
+
+function renderView(tweetView: TweetView) {
+  let view = document.querySelector("#container-" + tweetView.id);
+
+  if (view) {
+    view.innerHTML = "";
+  } else {
+    view = document.createElement("div");
+    view.id = "container-" + tweetView.id;
+    view.classList.add("mainContainer");
+    document.querySelector("#tweets")?.append(view);
+  }
+
+  for (const tweet of tweetView.tweets) {
+    //renderTweet
+  }
+}
